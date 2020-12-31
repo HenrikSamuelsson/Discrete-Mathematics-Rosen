755eac36e7c5267a8f4b7cec2c88395b2c1d462a
Henrik Samuelsson <henrik.samuelsson@ka-group.com>
2020-12-31 13:53:14 +0100

    Bug fix and new functions to print results.

--- chapter-01/computer-projects/project-1/src/truth_values.cpp
+++ chapter-01/computer-projects/project-1/src/truth_values.cpp
@@ -7,28 +7,31 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
 
+void printProjectInfo(void);
 void printTruthValue(bool value);
+void printHelpSection(void);
+void printHeader(void);
+
+bool conjunction(bool s, bool t);
 
 int main()
 {
 	bool p;
 	bool q;
-	bool conjunction;
 	
+	printProjectInfo();
+	printHelpSection();
+	printHeader();
+
 	for (int i = 0; i < 4; i++)
 	{
-		p = ( i >> 0 ) & 0x01;
-		q = ( i >> 1 ) & 0x01;
-	
-		bool conjunction;
-
+		q = ( i >> 0 ) & 0x01;
+		p = ( i >> 1 ) & 0x01;
 		printTruthValue(p);
 		printTruthValue(q);
-		conjunction = p && q;
-		printTruthValue(conjunction);
-		cout << endl;
+		printTruthValue(conjunction(p, q));
+		std::cout << std::endl;
 	}
 
 	return 0;
@@ -38,11 +41,35 @@ void printTruthValue(bool value)
 {
 	if (value)
 		{
-			cout << "T ";
+			std::cout << "T ";
 		}
 		else
 		{
-			cout << "F ";
+			std::cout << "F ";
 		}
 }
 
+bool conjunction(bool s, bool t)
+{
+	return s && t;
+}
+
+void printHelpSection(void)
+{
+	std::cout << "How to read the table: " << std::endl;
+	std::cout << "The given propositions are p and q." << std::endl;
+	std::cout << "Conjunction is indicated by &." << std::endl;
+	std::cout << std::endl;
+}
+
+void printHeader(void)
+{
+	std::cout << "p q &" << std::endl;
+}
+
+void printProjectInfo(void)
+{
+	std::cout << "Discrete Mathematics and Its Applications by Kenneth H. Rosen" << std::endl;
+	std::cout << "Chapter 1 - Computer projects - Project 1." << std::endl;
+	std::cout << std::endl;
+}
